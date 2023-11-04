0c755d45f80292711aa07b8b18442c8f359d7a03
Fabio Costantino <fabiocostantino84@gmail.com>
2023-11-04 17:37:03 +0100

    add fixed name link contact

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,7 +3,7 @@
         <a class="nav-link active" href="#" aria-current="page">Home</a>
     </li>
     <li class="nav-item">
-        <a class="nav-link" href="#">Contacts</a>
+        <a class="nav-link" href="#">Contact</a>
     </li>
     <li class="nav-item">
         <a class="nav-link" href="#">About</a>
