98dc39568064aa361eae00938d1cd6ac80260e4d
Fabio Costantino <fabiocostantino84@gmail.com>
2023-11-04 13:34:50 +0100

    add faker/gen and for cicle

--- database/seeders/TaskSeeder.php
+++ database/seeders/TaskSeeder.php
@@ -2,16 +2,26 @@
 
 namespace Database\Seeders;
 
+use App\Models\Task;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker; // lo inniettiamo nel metodo run(Faker $faker)
 
 class TaskSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        //
+        for ($i=0; $i < 10 ; $i++) { 
+            // creo prima un nuova variabile
+            $task = new Task(); // se dà questo errore(Undefined type 'Database\Seeders\Task') bisogna aggiungere lo 'use' con il percorso (use App\Models\Task;)
+
+            $task->description = $faker->realText(100);
+
+            $task->save();
+
+        }
     }
 }
